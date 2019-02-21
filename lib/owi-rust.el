65ee687075ed43e1e59f56ca7466f58867316903
Oskar Wickström <oskar.wickstrom@gmail.com>
2019-02-21 08:31:24 +0100

    Fix the rust mode config

--- lib/owi-rust.el
+++ lib/owi-rust.el
@@ -1,20 +1,18 @@
 (require 'owi-flycheck)
 
+(defun my-rust-mode-hook ()
+  (flycheck-mode)
+  (flycheck-rust-setup))
+
 (use-package rust-mode
   :ensure t
-  :defer t
-  :init
-  (require 'rust-mode)
-  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+  :mode ("\\.rs\\'" . rust-mode)
 
   :config
-  (use-package flycheck-rust
-    :ensure t)
-
-  (defun my-rust-mode-hook ()
-    (flycheck-mode)
-    (cargo-minor-mode)
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-  (add-hook 'rust-mode-hook 'my-rust-mode-hook))
+  (use-package flycheck-rust :ensure t)
+  (use-package cargo :ensure t)
+  (add-hook 'rust-mode-hook #'my-rust-mode-hook)
+  (add-hook 'rust-mode-hook 'cargo-minor-mode))
 
 (provide 'owi-rust)
