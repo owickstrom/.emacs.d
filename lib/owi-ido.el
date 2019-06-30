03e7d777307a4ad5198634fb2e78b34d318e43df
Oskar Wickström <oskar@wickstrom.tech>
2019-06-30 11:03:18 +0200

    Disable auto-search in ido-find-file

--- lib/owi-ido.el
+++ lib/owi-ido.el
@@ -4,6 +4,18 @@
   :config
   (setq ido-enable-flex-matching t)
   (ido-everywhere t)
-  (ido-mode 1))
+  (ido-mode 1)
+
+  ;; disable auto searching for files unless called explicitly with C-c
+  ;; C-s
+  ;;
+  ;; from https://superuser.com/a/322790
+  (setq ido-auto-merge-delay-time 99999)
+  (define-key ido-file-dir-completion-map (kbd "C-c C-s")
+    (lambda()
+      (interactive)
+      (ido-initiate-auto-merge (current-buffer))))
+
+  )
 
 (provide 'owi-ido)
