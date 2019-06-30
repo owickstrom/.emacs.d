09990d46233fb78947ac731324c79d3ea240c74b
Oskar Wickström <oskar@wickstrom.tech>
2019-06-30 11:03:02 +0200

    Switch between *Test.hs file and impl with projectile

--- lib/owi-projectile.el
+++ lib/owi-projectile.el
@@ -6,6 +6,13 @@
   (projectile-mode +1)
   :bind-keymap
   ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-test-suffix-function 'my-projectile-test-suffix)
   )
+
+(defun my-projectile-test-suffix (project-type)
+  (cond
+   ((member project-type '(haskell-cabal haskell-stack)) "Test")
+   (t (projectile-test-suffix project-type))))
 
 (provide 'owi-projectile)
