2e40a1dce7ffb234e10b27c9478ea87831100b42
Oskar Wickström <oskar@wickstrom.tech>
2022-05-21 22:07:10 +0200

    Ocaml

--- lib/owi-ocaml.el
+++ lib/owi-ocaml.el
@@ -11,7 +11,21 @@
   (add-hook 'merlin-mode-hook #'company-mode)
   (setq merlin-error-after-save nil)
   (add-hook 'merlin-mode-hook (lambda () (flycheck-ocaml-setup)))
-  (add-hook 'merlin-mode-hook 'flycheck-mode))
+  (add-hook 'merlin-mode-hook 'flycheck-mode)
+
+  :bind
+  (:map merlin-mode-map
+  ("C-c t"   . merlin-type-enclosing)
+  ("C-c e"   . merlin-type-expr)
+  ("C-c i"   . merlin-switch-to-mli)
+  ("C-S-i"   . ocamlformat)
+
+  ("C-c C-z"   . utop)
+  ("C-c C-b"   . utop-eval-buffer)
+  ("C-c C-e"   . utop-eval-phrase)
+  ("C-c C-r"   . utop-eval-region)
+  )
+  )
 
 (use-package flycheck-ocaml
   :after merlin
@@ -19,6 +33,9 @@
   (flycheck-ocaml-setup))
 
 (use-package merlin-company
+  :after merlin)
+
+(use-package ocamlformat
   :after merlin)
 
 (provide 'owi-ocaml)
